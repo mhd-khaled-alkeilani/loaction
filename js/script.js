f5730278c47dbc8a7cf73edb33c1bdbdb52ff567
mhd-khaled-alkeilani <khaled.alkeilani@outlook.com>
2024-02-14 19:23:09 +0400

    test script

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// const successCallback = (position) => {
+//   console.log(position);
+// };
+
+// const errorCallback = (error) => {
+//   console.log(error);
+// };
+
+// navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+alert("hiii");
